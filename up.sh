56dfdd646854e2a8ded4ee5b68373b71df56788c
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-09-03 08:38:46 +0530

    Scripts + Dashboard Revamp

--- up.sh
+++ up.sh
@@ -4,32 +4,14 @@ CONFIG_JSON_PATH=$1
 
 # Function to set up cluster with the provided configuration.
 function set_up_cluster {
-  CONFIG_JSON_PATH=$1
-
   echo "##################################"
   echo "##                              ##"
   echo "##      Setting Up Cluster      ##"
   echo "##                              ##"
   echo "##################################"
 
-  # Extract cluster configuration.
-  CLUSTER_NAME=$(jq -rc '.cluster_name' "$CONFIG_JSON_PATH")
-  VCPU_PER_NODE=$(jq -rc '.cluster_configuration.vcpu_per_node' "$CONFIG_JSON_PATH")
-  RAM_PER_NODE=$(jq -rc '.cluster_configuration.ram_per_node' "$CONFIG_JSON_PATH")
-  NODES=$(jq -rc '.cluster_configuration.nodes' "$CONFIG_JSON_PATH")
-  DRIVER=$(jq -rc '.cluster_configuration.driver' "$CONFIG_JSON_PATH")
-
-  echo -e "\n\n\n\n# --------------- CLUSTER CONFIGURATION --------------- #\n\n"
-  echo "Name: $CLUSTER_NAME"
-  echo "vCPUs Per Node: $VCPU_PER_NODE"
-  echo "RAM Per Node: $RAM_PER_NODE"
-  echo "Number of Nodes: $NODES"
-  echo "Driver Selected: $DRIVER"
-
-  echo "Setting up cluster $CLUSTER_NAME ..."
-
   # Setup cluster using minikube.
-  minikube --profile "$CLUSTER_NAME" start --cpus "$VCPU_PER_NODE" --memory "$RAM_PER_NODE"GB --nodes "$NODES" --driver "$DRIVER"
+  k3d cluster create --config cluster.yml
 
   if [[ $? != 0 ]]
   then
@@ -52,44 +34,46 @@ function set_up_cluster {
   fi
 }
 
-function set_up_plugins {
-  CONFIG_JSON_PATH=$1
-  CLUSTER_NAME=$2
+function set_up_dashboard {
 
   echo "##################################"
   echo "##                              ##"
-  echo "##      Installing plugins      ##"
+  echo "##     Installing Dashboard     ##"
   echo "##                              ##"
   echo "##################################"
 
-  PLUGINS_LIST=$(jq -rc '.plugins[]' "$CONFIG_JSON_PATH")
+  helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+  helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
 
-  for PLUGIN in $PLUGINS_LIST
-  do
-    echo -e "\n\nSetting up $PLUGIN..."
-    minikube --profile "$CLUSTER_NAME" addons enable "$PLUGIN"
+  if [[ $? != 0 ]]
+  then
+    echo "##################################"
+    echo "##                              ##"
+    echo "## Installing Dashboard Failed  ##"
+    echo "## Please check the above logs! ##"
+    echo "##                              ##"
+    echo "##################################"
 
+    exit 1
+  else
 
-    if [[ $? != 0 ]]
-    then
-      echo "##################################"
-      echo "##                              ##"
-      echo "##  Installing Plugins Failed   ##"
-      echo "## Please check the above logs! ##"
-      echo "##                              ##"
-      echo "##################################"
+    kubectl apply -f config/serviceaccount.yml
+    kubectl apply -f config/clusterrolebinding.yml
 
-      exit 1
-    else
-    echo -e "\n\n$PLUGIN is installed"
-    fi
-  done
+    echo "##################################"
+    echo "##                              ##"
+    echo "## Setting Up Dashboard Success ##"
+    echo "##                              ##"
+    echo "##################################"
+
+    echo "Dashboard Token: $(kubectl -n kubernetes-dashboard create token admin-user)"
+
+  fi
 
 }
 
 function install_operators {
   CONFIG_JSON_PATH=$1
-  CLUSTER_NAME=$2
 
   echo "##################################"
   echo "##                              ##"
@@ -136,12 +120,12 @@ function install_operators {
 
 CLUSTER_NAME=$(jq -rc '.cluster_name' "$CONFIG_JSON_PATH")
 
-CHECK_CLUSTER_EXISTENCE=$(minikube profile list | grep -c "$CLUSTER_NAME" || true)
+CHECK_CLUSTER_EXISTENCE=$(k3d cluster list | grep -c "$CLUSTER_NAME" || true)
 
 if [[ "$CHECK_CLUSTER_EXISTENCE" == "0" ]]
 then
-  set_up_cluster "$CONFIG_JSON_PATH"
-  set_up_plugins "$CONFIG_JSON_PATH" "$CLUSTER_NAME"
+  set_up_cluster
+  set_up_dashboard
 fi
 
-install_operators "$CONFIG_JSON_PATH" "$CLUSTER_NAME"
+install_operators "$CONFIG_JSON_PATH"
