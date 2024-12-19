fa2eb3ea2434953075cfcd75bb7f20f296f9d442
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-12-19 11:06:22 +0530

    Providers Update (#36)
    * Operators Provider Update
    
    * Dashboard Providers Update
    
    * Certificates Provider Updates

--- operators/providers.tf
+++ operators/providers.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     kubernetes = {
-      source  = "opentofu/kubernetes"
-      version = "2.32.0"
+      source  = "hashicorp/kubernetes"
+      version = "2.35.0"
     }
     helm = {
-      source  = "opentofu/helm"
-      version = "2.15.0"
+      source  = "hashicorp/helm"
+      version = "2.16.1"
     }
   }
 
